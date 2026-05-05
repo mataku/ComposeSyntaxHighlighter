249c40d1066593d4347992730171e7d53647f8eb
Takuma Homma <nagomimatcha@gmail.com>
2026-05-05 21:34:48 +0900

    feat(core-api): add InternalSyntaxHighlightApi opt-in marker

---
+++ core-api/src/commonMain/kotlin/io/github/mataku/compose/highlight/api/InternalSyntaxHighlightApi.kt
@@ -0,0 +1,8 @@
+package io.github.mataku.compose.highlight.api
+
+@RequiresOptIn(
+    message = "Internal Compose Highlight SPI. Use only when implementing a language module.",
+    level = RequiresOptIn.Level.ERROR,
+)
+@Retention(AnnotationRetention.BINARY)
+annotation class InternalSyntaxHighlightApi
